155ccff54297703bb7a36f11bc7a055c5741809e
lalitraghu1c <raghuwanshilalit5@gmail.com>
2022-11-04 00:20:48 +0530

    [Lalit] Add: Folder Structure Created

---
+++ LogicalProblem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace LogicalProblem
+
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Logical Programming Problems");
+        }
+    }
+}
