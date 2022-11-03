fe32e5395e7dfff4dcf861eea32e6886949e1818
lalitraghu1c <raghuwanshilalit5@gmail.com>
2022-11-04 00:49:16 +0530

    [Lalit] Add: Coupon Number

--- LogicalProblem/Program.cs
+++ LogicalProblem/Program.cs
@@ -10,7 +10,7 @@ namespace LogicalProblem
             while (flag)
             {
                 Console.WriteLine("Welcome to Logical Programming Problems");
-                Console.WriteLine("1. Fibonaccis Series" + "\n" + "2. Perfect Number" + "\n" + "3. Prime Number " + "\n" + "4. Reverse Number " + "\n" + "5. Exit");
+                Console.WriteLine("1. Fibonaccis Series" + "\n" + "2. Perfect Number" + "\n" + "3. Prime Number " + "\n" + "4. Reverse Number " + "\n" + "5. Coupon Number" + "\n" + "6. Exit");
                 int check = Convert.ToInt32(Console.ReadLine());
                 switch (check)
                 {
@@ -30,6 +30,9 @@ namespace LogicalProblem
                         ReverseNumber reverse = new ReverseNumber();
                         reverse.Reverse(1432);
                         break;
+                    case 5:
+                        CouponNumber.GenerateCoupon(10);
+                        break ;
                     default:
                         flag = false;
                         break;
