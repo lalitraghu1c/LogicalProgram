3ca763b6cda6f490b53c31bbba7395af38a58803
lalitraghu1c <raghuwanshilalit5@gmail.com>
2022-11-04 00:26:28 +0530

    [Lalit] Add: Fibonacci Series

--- LogicalProblem/Program.cs
+++ LogicalProblem/Program.cs
@@ -6,7 +6,23 @@ namespace LogicalProblem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Logical Programming Problems");
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Welcome to Logical Programming Problems");
+                Console.WriteLine("1. Fibonaccis Series" + "\n" + "2. Exit");
+                int check = Convert.ToInt32(Console.ReadLine());
+                switch (check)
+                {
+                    case 1:
+                        FibonacciSeries fibo = new FibonacciSeries();
+                        fibo.Fibonacci(20);
+                        break;
+                        default:
+                        flag = false;
+                        break;
+                }
+            }
         }
     }
 }
